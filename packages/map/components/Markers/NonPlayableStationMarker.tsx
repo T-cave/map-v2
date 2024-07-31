42810d0ce3873f12b638a8e827238569115ec84d
T-cave <tijnvanerven@gmail.com>
2024-07-31 21:03:44 +0200

    fixed failed quality issues 2

--- packages/map/components/Markers/NonPlayableStationMarker.tsx
+++ packages/map/components/Markers/NonPlayableStationMarker.tsx
@@ -13,10 +13,12 @@ export const NonPlayableStationMarker = ({ station }: StationMarkerProps) => {
 		popupAnchor: [0, -16],
 	});
 
-    let displayText = station.Name
+    let displayText
     if (station.Prefix !== "") {
-        displayText += ` [` + station.Prefix + `]`
-    }
+        displayText = station.Name
+    } else {
+		displayText = station.Name + " [" + station.Prefix + "]"
+	}
 
 	return (
 		<Marker
